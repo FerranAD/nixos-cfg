3bf094014d9086648aedbdeba127c6de4467a50d
FerranAD <github@ferranaran.com>
2024-09-08 21:15:48 +0200

    feat(nvidia): add nvidia module

---
+++ modules/nixos/nvidia.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  config,
+  ... 
+}: {
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+  };
+
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+}
