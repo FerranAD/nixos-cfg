4afe7d4d8ef6e26daae33f9fab43bc5ff5709c9a
FerranAD <github@ferranaran.com>
2024-09-22 18:27:26 +0200

    feat(hyprpanel): add battery widget dependencies

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -7,6 +7,12 @@
   programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
 
   services.dbus.enable = true;
+  services.gvfs.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
+
+  services.logind.lidSwitch = "ignore";
+  services.logind.lidSwitchExternalPower = "ignore";
 
   xdg.portal = {
     enable = true;
