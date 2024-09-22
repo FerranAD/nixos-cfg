2b074517646b0013129a4689403fa5b74e9eedc5
FerranAD <github@ferranaran.com>
2024-09-22 18:28:38 +0200

    feat(hyprland): make hyprpanel responsive to monitors

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -1,7 +1,23 @@
 
-{ pkgs, inputs, ... }:
+{ pkgs, lib, inputs, ... }:
 {
-  wayland.windowManager.hyprland = {
+  wayland.windowManager.hyprland = 
+    let
+      restartHyprpanel = pkgs.pkgs.writeShellScriptBin "restartHyprpanel" ''
+        "${pkgs.hyprpanel}/bin/hyprpanel" -q
+        "${pkgs.hyprpanel}/bin/hyprpanel"
+      '';
+      monitorHotplugCallback = pkgs.pkgs.writeShellScriptBin "monitorHotplugCallback" ''
+        handle() {
+          case $1 in
+            monitoradded*) ${lib.getExe restartHyprpanel} ;;
+          esac
+        }
+
+        "${pkgs.socat}" -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done 
+      '';
+    in
+  {
     enable = true;
     # plugins = [
     #   inputs.hyprland-plugins.packages."${pkgs.system}".borders-plus-plus
@@ -23,8 +39,13 @@
         "$mod, mouse:272, movewindow"
         "$mod, mouse:273, resizewindow"
       ];
+      bindl = [
+        ", switch:on:Lid Switch,exec,hyprctl keyword monitor \"eDP-1, disable\""
+        ", switch:off:Lid Switch,exec,hyprctl keyword monitor \"eDP-1,1920x1080@144.00,0x0,0\" && ${lib.getExe restartHyprpanel}"
+      ];
       exec-once = [
         "${pkgs.hyprpanel}/bin/hyprpanel"
+        "${lib.getExe monitorHotplugCallback}"
       ];
     };
   };
