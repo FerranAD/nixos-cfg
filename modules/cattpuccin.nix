6e9749fdf8b03e05ad004310354ffee4c0bba2da
FerranAD <github@ferranaran.com>
2024-10-10 20:26:48 +0200

    feat(catppuccin): add catppuccin theme

---
+++ modules/cattpuccin.nix
@@ -0,0 +1,30 @@
+{ inputs, ... }:
+let
+  flavor = "mocha";
+  user = "ferran";
+in
+{
+  imports = [
+    inputs.catppuccin.nixosModules.catppuccin
+    {
+      catppuccin.flavor = flavor;
+      catppuccin.enable = true;
+      boot.plymouth.catppuccin.enable = true;
+      services.displayManager.sddm.catppuccin.enable = false;
+    }
+    {
+      home-manager.users.${user} = {
+        imports = [
+          inputs.catppuccin.homeManagerModules.catppuccin
+        ];
+        catppuccin.flavor = flavor;
+        catppuccin.enable = true;
+        catppuccin.pointerCursor.enable = true;
+        catppuccin.pointerCursor.accent = "dark";
+        programs.fuzzel.catppuccin.enable = true;
+        # Disable since it's horrible on zsh
+        programs.zsh.syntaxHighlighting.catppuccin.enable = false;
+      };
+    }
+  ];
+}
