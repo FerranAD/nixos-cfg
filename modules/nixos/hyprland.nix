4aa0c0d2083f38edb61d68008a5dae3f9aed2d4f
FerranAD <github@ferranaran.com>
2024-10-12 17:36:01 +0200

    refactor(xdg-portal): extract xdg-portal config

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -7,14 +7,13 @@
   programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
 
   # For hyprpanel battery widget to work
-  services.dbus.enable = true;
   services.gvfs.enable = true;
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
-
   services.logind.lidSwitch = "ignore";
   services.logind.lidSwitchExternalPower = "ignore";
 
+  services.dbus.enable = true;
   xdg.portal = {
     enable = true;
     wlr.enable = true;
---
+++ modules/nixos/xdg-portal.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  ... 
+}: {
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+}
