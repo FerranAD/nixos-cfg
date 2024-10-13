56e60c208234ccbb074a1cbeebb95df95fcfb91c
FerranAD <github@ferranaran.com>
2024-10-13 15:29:25 +0200

    refactor(fuzzle): add vim-like keybindings

--- modules/home-manager/fuzzel.nix
+++ modules/home-manager/fuzzel.nix
@@ -6,6 +6,13 @@
       main = {
         exit-on-keyboard-focus-loss = "no";
       };
+      key-bindings = {
+        # Remaped since it defaults to Control+k
+        delete-line-forward= "Control+d";
+
+        next = "Down Control+j";
+        prev = "Up Control+k";
+      };
     };
   };
 }
