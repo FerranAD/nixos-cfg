e2ef22e5e50d064245eac40972b80d823644af84
Ferran Aran <ferran@ferranaran.com>
2025-01-01 19:49:22 +0100

    feat(nix-settings): add cuda cachix

--- modules/nixos/nix-settings.nix
+++ modules/nixos/nix-settings.nix
@@ -13,12 +13,14 @@
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://cache.soopy.moe"
+        "https://cuda-maintainers.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "hydra.soopy.moe:IZ/bZ1XO3IfGtq66g+C85fxU/61tgXLaJ2MlcGGXU8Q="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
       experimental-features = [
         "nix-command"
