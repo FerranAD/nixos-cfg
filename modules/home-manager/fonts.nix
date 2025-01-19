6459bc6c9877a0b669f018c619ff08a840522f02
Ferran Aran <ferran@ferranaran.com>
2025-01-19 16:39:08 +0100

    fix(fonts): set nerd fonts for all families

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -7,20 +7,24 @@
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
     nerd-fonts.iosevka
+    nerd-fonts.arimo
   ];
   fonts = {
     fontconfig = {
       enable = true;
       defaultFonts = {
         serif = [
+          "IosevkaTermSlab Nerd Font"
           "Noto Serif"
           "Noto Color Emoji"
         ];
         sansSerif = [
+          "Arimo Nerd Font"
           "Noto Sans"
           "Noto Color Emoji"
         ];
         monospace = [
+          "FiraMono Nerd Font"
           "JetBrainsMono Nerd Font"
           "Noto Color Emoji"
         ];
