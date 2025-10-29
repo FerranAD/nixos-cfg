e49f90189d7bd6d1d37739e2810643040f2a9b11
Ferran Aran <ferran@ferranaran.com>
2025-10-29 11:20:19 +0100

    feat(hedwig): add nftables config

--- hosts/hedwig/configuration.nix
+++ hosts/hedwig/configuration.nix
@@ -33,7 +33,7 @@
     wget
     git
     gnumake
-    lolcat 
+    lolcat
   ];
 
   boot = {
@@ -53,12 +53,45 @@
   hardware.enableRedistributableFirmware = true;
 
   networking = {
+    firewall.enable = false;
     hostName = "hedwig";
     networkmanager.enable = true;
     wireless = {
       interfaces = [ "wlan0" ];
       enable = false;
     };
+    nftables =
+      let
+        wanIf = "end0";
+        tailscale_ip_client = "100.78.176.53";
+        portLow = "18000";
+        portHigh = "34000";
+      in
+      {
+        enable = true;
+        
+        ruleset = ''
+          table ip nat {
+            chain prerouting {
+              type nat hook prerouting priority 0;
+
+              # Forward TCP + UDP port range from A's WAN -> B via Tailscale
+              iifname "${wanIf}" tcp dport ${portLow}-${portHigh} dnat to ${tailscale_ip_client}
+              iifname "${wanIf}" udp dport ${portLow}-${portHigh} dnat to ${tailscale_ip_client}
+            }
+
+            chain postrouting {
+              type nat hook postrouting priority 100;
+
+              # SNAT on the tailscale leg so A accepts the packets (appears from B’s 100.x)
+              oifname "tailscale0" ip daddr ${tailscale_ip_client} tcp dport ${portLow}-${portHigh} masquerade
+              oifname "tailscale0" ip daddr ${tailscale_ip_client} udp dport ${portLow}-${portHigh} masquerade
+
+              oifname "${wanIf}" masquerade
+            }
+          }
+        '';
+      };
   };
 
   nix.settings.experimental-features = [
