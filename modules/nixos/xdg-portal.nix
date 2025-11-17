177f4537a2daaa7de2c35c7eeb358e836e9516c0
Ferran Aran <ferran@ferranaran.com>
2025-11-17 10:00:52 +0100

    fix: desktop portal

--- modules/nixos/xdg-portal.nix
+++ modules/nixos/xdg-portal.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
   services.dbus.enable = true;
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    xdgOpenUsePortal = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  };
+  # xdg.portal = {
+  #   enable = true;
+  #   wlr.enable = true;
+  #   xdgOpenUsePortal = true;
+  #   extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  # };
 }
