e3bf2a57445b1d8d4294797d4ac6c2fe97e04caf
Ferran Aran <ferran@ferranaran.com>
2025-12-15 11:16:32 +0100

    feat(cache): add cuda cache

--- modules/nixos/nix-settings.nix
+++ modules/nixos/nix-settings.nix
@@ -7,22 +7,23 @@
   nix = {
     settings = {
       max-jobs = 6;
-      cores    = 0;
+      cores = 0;
       auto-optimise-store = true;
       allowed-users = [ "@wheel" ];
+      trusted-users = [ "@wheel" ];
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://cache.soopy.moe"
-        "https://cuda-maintainers.cachix.org"
+        "https://cache.nixos-cuda.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "hydra.soopy.moe:IZ/bZ1XO3IfGtq66g+C85fxU/61tgXLaJ2MlcGGXU8Q="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
       experimental-features = [
         "nix-command"
