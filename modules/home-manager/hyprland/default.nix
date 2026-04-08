2c346de34b04d085d44609d762ffa06059d167ce
Ferran Aran Domingo <ferran@aranferran.com>
2026-04-08 11:51:15 +0200

    fix(hyprland): fix plugin

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -69,17 +69,17 @@
         };
       };
 
-      plugin = {
-        hyprexpo = {
-          columns = 3;
-          gap_size = 4;
-          bg_col = "rgb(000000)";
+      # plugin = {
+      #   hyprexpo = {
+      #     columns = 3;
+      #     gap_size = 4;
+      #     bg_col = "rgb(000000)";
 
-          enable_gesture = true;
-          gesture_distance = 300;
-          gesture_positive = false;
-        };
-      };
+      #     enable_gesture = true;
+      #     gesture_distance = 300;
+      #     gesture_positive = false;
+      #   };
+      # };
 
       animations = {
         enabled = true;
