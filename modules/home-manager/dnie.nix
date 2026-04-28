0f9591ceed1e6760963a2dbefc1fac6ec3e07407
Ferran Aran Domingo <ferran@aranferran.com>
2026-04-28 17:44:15 +0200

    fix(dnie): fix dnie

--- modules/home-manager/dnie.nix
+++ modules/home-manager/dnie.nix
@@ -7,6 +7,13 @@
     inputs.autofirma-nix.homeManagerModules.default
   ];
 
+  programs.configuradorfnmt.enable = true;
+  programs.configuradorfnmt.firefoxIntegration.profiles = {
+    ferran = {
+      enable = true;
+    };
+  };
+
   programs.autofirma.enable = true;
   programs.autofirma.firefoxIntegration.profiles = {
     ferran = {
