1490af52639970d13e8775b4311e74313fdc3c34
Ferran Aran Domingo <ferran@aranferran.com>
2026-09-23 17:04:54 +0200

    fix(ollama): make it run on the gpu agani

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -4,6 +4,12 @@
     enable = true;
     host = "0.0.0.0";
     package = pkgs.ollama-vulkan;
+    environmentVariables = {
+      OLLAMA_IGPU_ENABLE = "1";
+      OLLAMA_VULKAN = "1";
+      OLLAMA_DEBUG = "1";
+      GGML_VK_VISIBLE_DEVICES = "0";
+    };
     loadModels = [
       "qwen3:8b"
       "qwen3:4b-instruct-2507-q8_0"
